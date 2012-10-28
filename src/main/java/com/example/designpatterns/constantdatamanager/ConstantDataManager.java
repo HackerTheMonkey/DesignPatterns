1261e79b38e3eb5d9daf5487393d89ed995920d7
hasanein <hasanein.ali@gmail.com>
2012-10-28 01:38:18 +0000

    making the ConstantDataManager and the related inner classes as final to prevent further subclassing

--- src/main/java/com/example/designpatterns/constantdatamanager/ConstantDataManager.java
+++ src/main/java/com/example/designpatterns/constantdatamanager/ConstantDataManager.java
@@ -1,14 +1,14 @@
 package com.example.designpatterns.constantdatamanager;
 
-public class ConstantDataManager 
+public final class ConstantDataManager 
 {	
-	public static class Account
+	public final class Account
 	{
 		static final String ACCOUNT_DATA_FILE = "ACCOUNT.TXT";
 		static final int VALID_MIN_LNAME_LEN = 2;	
 	}
 	
-	public static class Address
+	public final class Address
 	{
 		final static String ADDRESS_DATA_FILE = "ADDRESS.TXT";
 		final static String VALID_ZIP_CHARS = "0123456789";
@@ -16,7 +16,7 @@ public class ConstantDataManager
 		public static final int VALID_STREET_LENGTH = 2;
 	}
 	
-	public static class CreditCard
+	public final class CreditCard
 	{
 		static final String CC_DATA_FILE = "CC.TXT";
 		static final String VALID_CC_CHARS = "0123456789";
