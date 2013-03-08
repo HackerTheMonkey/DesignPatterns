2b886b9c3d2d2aa0bc4c32807027a03758b54418
hasanein <hasanein.ali@gmail.com>
2013-03-08 11:32:22 +0000

    Adding some fluent flavor to the decorator pattern implementation

---
+++ src/test/java/com/example/designpatterns/decorator/LoggerFluentFactory.java
@@ -0,0 +1,36 @@
+package com.example.designpatterns.decorator;
+
+public class LoggerFluentFactory
+{
+    private Logger logger;
+
+    public LoggerFluentFactory withHtmlDecoration() {
+        logger = new HtmlDecorator(logger);
+        return this;
+    }
+
+    public Logger build() {
+        return logger;
+    }
+
+    public LoggerFluentFactory toConsole()
+    {
+        logger = new ConsoleLogger();
+        return this;
+    }
+
+    public LoggerFluentFactory toFile() {
+        logger = new FileLogger();
+        return this;
+    }
+
+    public LoggerFluentFactory withReverseEncryptionDecoration() {
+        logger = new ReverseEncryptionDecorator(logger);
+        return this;
+    }
+
+    public LoggerFluentFactory withAbbreviatedDecorator() {
+        logger = new AbbreviatedDecorator(logger);
+        return this;
+    }
+}
---
+++ src/test/java/com/example/designpatterns/decorator/LoggersTestWithFluentFactory.java
@@ -0,0 +1,86 @@
+package com.example.designpatterns.decorator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class LoggersTestWithFluentFactory
+{
+
+    public static final String SAMPLE_MESSAGE = "sample message";
+    private FileLogger fileLogger;
+    private ConsoleLogger consoleLogger;
+    private LoggerFluentFactory loggerFluentFactory;
+
+    @Before
+    public void setup()
+    {
+        this.fileLogger = new FileLogger();
+        this.consoleLogger = new ConsoleLogger();
+        this.loggerFluentFactory = new LoggerFluentFactory();
+    }
+
+    @Test
+    public void create_system_under_test()
+    {
+        assertThat(fileLogger, is(notNullValue()));
+        assertThat(consoleLogger, is(notNullValue()));
+    }
+
+    @Test
+    public void should_log_message_to_file()
+    {
+        fileLogger.log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_message_to_console()
+    {
+        consoleLogger.log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_html_message_to_console()
+    {
+        loggerFluentFactory.toConsole().withHtmlDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_html_message_to_file()
+    {
+        loggerFluentFactory.toFile().withHtmlDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_encrypted_message_to_console()
+    {
+        loggerFluentFactory.toConsole().withReverseEncryptionDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_encrypted_message_to_file()
+    {
+        loggerFluentFactory.toConsole().withReverseEncryptionDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_encrypted_html_message_to_console()
+    {
+        loggerFluentFactory.toConsole().withReverseEncryptionDecoration().withHtmlDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_encrypted_html_message_to_file()
+    {
+        loggerFluentFactory.toFile().withReverseEncryptionDecoration().withHtmlDecoration().build().log(SAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void should_log_html_abbreviated_encrypted_message_to_console()
+    {
+        loggerFluentFactory.toConsole().withAbbreviatedDecorator().withReverseEncryptionDecoration().withHtmlDecoration().build().log(SAMPLE_MESSAGE);
+    }
+}
