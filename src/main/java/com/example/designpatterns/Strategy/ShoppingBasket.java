9f286bfffe15ef60594fdd7bfb564c9993059c9f
Joseph Khafaji <hasanein.ali@gmail.com>
2015-05-31 16:09:09 +0100

    Corrected a package name that caused a compilation error within the strategy pattern implementation

--- src/main/java/com/example/designpatterns/Strategy/ShoppingBasket.java
+++ src/main/java/com/example/designpatterns/Strategy/ShoppingBasket.java
@@ -3,9 +3,9 @@ package com.example.designpatterns.strategy;
 import com.example.designpatterns.strategy.payments.PaymentStrategy;
 
 public class ShoppingBasket {
-    private Item item;
+    private com.example.designpatterns.strategy.Item item;
 
-    public void addItem(Item item) {
+    public void addItem(com.example.designpatterns.strategy.Item item) {
         this.item = item;
     }
 
